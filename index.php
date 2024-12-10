0fc655043722c916d328ebf6b4302f615adf37e7
Mitriy_Bug <verstkajoom@gmail.com>
2024-12-10 22:35:39 +0300

    php-03 operators

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ echo "Отчество: ";
 $patronymic = fgets(STDIN);
 
 $fullName = $lastname . ' ' . $name . ' ' . $patronymic;
-$fullName = ucwords($fullName);
+$fullName = mb_convert_case($fullName, MB_CASE_TITLE, "UTF-8");
 echo 'Полное имя: ' . $fullName . PHP_EOL;
 
-$surnameAndInitials = ucfirst($lastname) . ' ' . ucfirst(mb_substr($name, 0, 1)) . '.' . ucfirst(mb_substr($patronymic, 0, 1)). '.';
+$surnameAndInitials = mb_convert_case($lastname, MB_CASE_TITLE, "UTF-8") . ' ' . mb_convert_case(mb_substr($name, 0, 1), MB_CASE_TITLE, "UTF-8") . '.' . mb_convert_case(mb_substr($patronymic, 0, 1), MB_CASE_TITLE, "UTF-8"). '.';
 echo 'Фамилия и инициалы: ' . $surnameAndInitials . PHP_EOL;
 //mb_ucfirst() - не работает???
 
@@ -21,3 +21,5 @@ echo 'Аббревиатура: ' . $fio . PHP_EOL;
 //Такое преобразование не помогает вывести кириллические символы
 //echo "Кодировка введенной строки: " . mb_detect_encoding($lastname) . PHP_EOL;
 //echo mb_convert_encoding($lastname,'UTF-8', mb_detect_encoding($lastname));
+
+//mb_convert_case(mb_substr($patronymic, 0, 1), MB_CASE_TITLE, "UTF-8")
